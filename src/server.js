4e706ff02bde3b407a6f7210a4a84485937be2bb
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2022-09-14 11:07:44 -0300

    initial structural project

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+    // const path = require('path')
+
+// const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+// db.connect()
+
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', routes)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server running in port ${port}.`)
+})
